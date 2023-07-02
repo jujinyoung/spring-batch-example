fa3acaa087daaccc698be0de75c1f5052130bf1a
jujinyoung <jujinyoung1838@naver.com>
2023-07-02 10:57:57 +0900

    스프링 배치 실행 - Chunk

---
+++ src/main/java/io/spring/batch/springbatch/chunk/chunk1/ChunkConfiguration1.java
@@ -0,0 +1,73 @@
+package io.spring.batch.springbatch.chunk.chunk1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class ChunkConfiguration1 {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job chunkJob1() {
+        return jobBuilderFactory.get("chunkJob1")
+                .incrementer(new RunIdIncrementer())
+                .start(chunk1Step1())
+                .next(chunk1Step2())
+                .build();
+    }
+
+    @Bean
+    public Step chunk1Step1() {
+        return stepBuilderFactory.get("chunk1Step1")
+                .<String, String>chunk(5)
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        Thread.sleep(300);
+                        System.out.println("item = " + item);
+                        return "my" + item;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+                        Thread.sleep(300);
+                        System.out.println("items = " + items);
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step chunk1Step2() {
+        return stepBuilderFactory.get("chunk1Step2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step2 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
--- src/main/java/io/spring/batch/springbatch/step/advanced/StepBuilderConfiguration.java
+++ src/main/java/io/spring/batch/springbatch/step/advanced/StepBuilderConfiguration.java
@@ -1,131 +1,131 @@
-package io.spring.batch.springbatch.step.advanced;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.*;
-import org.springframework.batch.item.support.ListItemReader;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
-import java.util.List;
-
-
-@RequiredArgsConstructor
-@Configuration
-public class StepBuilderConfiguration {
-
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job batchJob(){
-        return jobBuilderFactory.get("batchJob")
-                .incrementer(new RunIdIncrementer())
-                .start(step1())
-                .next(step2())
-                .next(step3())
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println(" =========================");
-                    System.out.println(" >> step1 Spring Batch!!");
-                    System.out.println(" =========================");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .<String, String>chunk(3)
-                .reader(new ItemReader<String>() {
-                    @Override
-                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-                        return null;
-                    }
-                })
-                .processor(new ItemProcessor<String, String>() {
-                    @Override
-                    public String process(String s) throws Exception {
-                        return null;
-                    }
-                })
-                .writer(new ItemWriter<String>() {
-                    @Override
-                    public void write(List<? extends String> list) throws Exception {
-
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .partitioner(step1())
-                .gridSize(2)
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .job(job())
-                .build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .flow(flow())
-                .build();
-    }
-
-    @Bean
-    public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
-                .next(step3())
-                .build();
-    }
-
-    @Bean
-    public Flow flow() {
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
-        flowBuilder.start(step2()).end();
-        return flowBuilder.build();
-    }
-
-    @Bean
-    public Step chunkStep() {
-        return stepBuilderFactory.get("chunkStep")
-                .<String, String>chunk(10)
-                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
-                .processor(new ItemProcessor<String, String>() {
-                    @Override
-                    public String process(String item) throws Exception {
-                        return item.toUpperCase();
-                    }
-                })
-                .writer(new ItemWriter<String>() {
-                    @Override
-                    public void write(List<? extends String> items) throws Exception {
-                        items.forEach(item -> System.out.println(item));
-                    }
-                })
-                .build();
-    }
-}
+//package io.spring.batch.springbatch.step.advanced;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.*;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.job.builder.FlowBuilder;
+//import org.springframework.batch.core.job.flow.Flow;
+//import org.springframework.batch.core.launch.support.RunIdIncrementer;
+//import org.springframework.batch.item.*;
+//import org.springframework.batch.item.support.ListItemReader;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//
+//@RequiredArgsConstructor
+//@Configuration
+//public class StepBuilderConfiguration {
+//
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job StepBuilderJob(){
+//        return jobBuilderFactory.get("StepBuilderJob")
+//                .incrementer(new RunIdIncrementer())
+//                .start(StepBuilderStep1())
+//                .next(StepBuilderStep2())
+//                .next(StepBuilderStep3())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step StepBuilderStep1() {
+//        return stepBuilderFactory.get("StepBuilderStep1")
+//                .tasklet((stepContribution, chunkContext) -> {
+//                    System.out.println(" =========================");
+//                    System.out.println(" >> step1 Spring Batch!!");
+//                    System.out.println(" =========================");
+//                    return RepeatStatus.FINISHED;
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step StepBuilderStep2() {
+//        return stepBuilderFactory.get("StepBuilderStep2")
+//                .<String, String>chunk(3)
+//                .reader(new ItemReader<String>() {
+//                    @Override
+//                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+//                        return null;
+//                    }
+//                })
+//                .processor(new ItemProcessor<String, String>() {
+//                    @Override
+//                    public String process(String s) throws Exception {
+//                        return null;
+//                    }
+//                })
+//                .writer(new ItemWriter<String>() {
+//                    @Override
+//                    public void write(List<? extends String> list) throws Exception {
+//
+//                    }
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step StepBuilderStep3() {
+//        return stepBuilderFactory.get("StepBuilderStep3")
+//                .partitioner(StepBuilderStep1())
+//                .gridSize(2)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step StepBuilderStep4() {
+//        return stepBuilderFactory.get("StepBuilderStep4")
+//                .job(job123())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step StepBuilderStep5() {
+//        return stepBuilderFactory.get("StepBuilderStep5")
+//                .flow(flow())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Job job123() {
+//        return this.jobBuilderFactory.get("job123")
+//                .start(StepBuilderStep1())
+//                .next(StepBuilderStep2())
+//                .next(StepBuilderStep3())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Flow flow() {
+//        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+//        flowBuilder.start(StepBuilderStep2()).end();
+//        return flowBuilder.build();
+//    }
+//
+//    @Bean
+//    public Step chunkStep() {
+//        return stepBuilderFactory.get("chunkStep")
+//                .<String, String>chunk(10)
+//                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+//                .processor(new ItemProcessor<String, String>() {
+//                    @Override
+//                    public String process(String item) throws Exception {
+//                        return item.toUpperCase();
+//                    }
+//                })
+//                .writer(new ItemWriter<String>() {
+//                    @Override
+//                    public void write(List<? extends String> items) throws Exception {
+//                        items.forEach(item -> System.out.println(item));
+//                    }
+//                })
+//                .build();
+//    }
+//}
